4d73f61076b2809d9560131c2faef4d56ed35b26
JJGantt <jaredgantt@gmail.com>
2025-01-13 17:45:10 -0500

    bt

--- index.js
+++ index.js
@@ -133,5 +133,5 @@ app.post('/api/vote', async (req, res) => {
   
 
 app.listen(port, () => {
-  console.log('Server is running on port ${PORT}');
+  console.log(`Server is running on port ${PORT}`);
 });
